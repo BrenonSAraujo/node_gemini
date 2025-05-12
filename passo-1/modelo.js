3d72115645ded338ef511434e8dbb9b7f157d77c
BrenonSAraujo <brenonsilvestre64@gmail.com>
2025-05-12 02:12:51 -0300

    Projeto adicionado

---
+++ passo-1/modelo.js
@@ -0,0 +1,8 @@
+import { GoogleGenerativeAI } from '@google/generative-ai';
+import 'dotenv/config'
+
+export async function inicializaModelo(modelo) {
+  const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
+  const model = genAI.getGenerativeModel({ model: modelo});
+  return model;
+}
